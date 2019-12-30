7d7e8b870241eff6a7bc14cf8ef30c1ac0f9b904
Faris Ghilmany <ceeros99@gmail.com>
2019-12-30 10:58:00 +0700

    Popular Item Done

--- app/src/main/java/com/dicoding/picodiploma/tmdbapplication/fragment/PopularFragment.kt
+++ app/src/main/java/com/dicoding/picodiploma/tmdbapplication/fragment/PopularFragment.kt
@@ -3,15 +3,29 @@ package com.dicoding.picodiploma.tmdbapplication.fragment
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.dicoding.picodiploma.tmdbapplication.R
+import com.dicoding.picodiploma.tmdbapplication.adapter.MovieAdapter
+import com.dicoding.picodiploma.tmdbapplication.api.ApiRepository
+import com.dicoding.picodiploma.tmdbapplication.model.Movie
+import com.dicoding.picodiploma.tmdbapplication.presenter.MoviePresenter
+import com.dicoding.picodiploma.tmdbapplication.view.MovieView
+import com.google.gson.Gson
+import kotlinx.android.synthetic.main.fragment_popular.*
 
-/**
- * A simple [Fragment] subclass.
- */
-class PopularFragment : Fragment() {
+
+class PopularFragment : Fragment(), MovieView {
+
+    private var movies : MutableList<Movie> = mutableListOf()
+    private lateinit var presenter : MoviePresenter
+    private lateinit var adapter: MovieAdapter
+
+    private lateinit var item:Movie
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +33,43 @@ class PopularFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_popular, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val request = ApiRepository()
+        val gson = Gson()
+        presenter = MoviePresenter(this, request, gson)
+        presenter.getPopularMovie()
+
+        adapter = MovieAdapter(movies){}
+        rv_popular.layoutManager = GridLayoutManager(activity,2) as RecyclerView.LayoutManager?
+        rv_popular.adapter = adapter
+
+    }
+
+    override fun showLoading() {
+
+    }
+
+    override fun hideLoading() {
+
+    }
+
+    override fun showMovie(data: List<Movie>) {
+
+        movies.clear()
+        movies.addAll(data)
+        adapter.notifyDataSetChanged()
+
+        item = Movie(
+            data[0].id,
+            data[0].judulFilm,
+            data[0].posterFilm
+        )
+
+
     }
 
 
